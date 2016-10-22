38960c7041377eb195dd51b854c63fc7e207b4e4
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-10-22 12:16:03 +0800

    Revert highlight colors, they look nasty.

--- library/src/main/java/me/zhanghai/android/materialratingbar/MaterialRatingDrawable.java
+++ library/src/main/java/me/zhanghai/android/materialratingbar/MaterialRatingDrawable.java
@@ -7,12 +7,9 @@ package me.zhanghai.android.materialratingbar;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
-import android.support.v4.graphics.ColorUtils;
 import android.support.v7.content.res.AppCompatResources;
 import android.view.Gravity;
 
@@ -35,30 +32,12 @@ public class MaterialRatingDrawable extends LayerDrawable {
 
     private static Drawable createLayerDrawable(int tileResId, boolean tintAsActivatedElseNormal,
                                                 Context context) {
+        int tintColor = ThemeUtils.getColorFromAttrRes(tintAsActivatedElseNormal ?
+                R.attr.colorControlActivated : R.attr.colorControlNormal, context);
         TiledDrawable drawable = new TiledDrawable(AppCompatResources.getDrawable(context,
                 tileResId));
-        int activatedColor = ThemeUtils.getColorFromAttrRes(R.attr.colorControlActivated, context);
-        if (tintAsActivatedElseNormal) {
-            //noinspection RedundantCast
-            ((TintableDrawable) drawable).setTint(activatedColor);
-        } else {
-            int normalColor = ThemeUtils.getColorFromAttrRes(R.attr.colorControlNormal, context);
-            float disabledAlpha = ThemeUtils.getFloatFromAttrRes(android.R.attr.disabledAlpha,
-                    context);
-            int highlightColor = ColorUtils.setAlphaComponent(activatedColor,
-                    Math.round(Color.alpha(activatedColor) * disabledAlpha));
-            ColorStateList tintList = new ColorStateList(new int[][] {
-                    new int[] { android.R.attr.state_focused },
-                    new int[] { android.R.attr.state_pressed },
-                    new int[] {}
-            }, new int[] {
-                    highlightColor,
-                    highlightColor,
-                    normalColor
-            });
-            //noinspection RedundantCast
-            ((TintableDrawable) drawable).setTintList(tintList);
-        }
+        //noinspection RedundantCast
+        ((TintableDrawable) drawable).setTint(tintColor);
         return drawable;
     }
 
