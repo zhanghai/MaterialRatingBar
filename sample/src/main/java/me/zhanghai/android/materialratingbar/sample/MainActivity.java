f8192d534fbadbfe2a0184460b1aa5357a9f55b7
Zhang Hai <dreaming.in.code.zh@gmail.com>
2016-10-22 17:39:46 +0800

    [Feature] Modify styles; Add decimal rating to sample.

--- sample/src/main/java/me/zhanghai/android/materialratingbar/sample/MainActivity.java
+++ sample/src/main/java/me/zhanghai/android/materialratingbar/sample/MainActivity.java
@@ -10,14 +10,31 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.Transformation;
+import android.widget.RatingBar;
+
+import butterknife.BindViews;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindViews({
+            R.id.framework_decimal_ratingbar,
+            R.id.library_decimal_ratingbar,
+            R.id.library_tinted_decimal_ratingbar
+    })
+    RatingBar[] mDecimalRatingBars;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
+        ButterKnife.bind(this);
+
+        mDecimalRatingBars[0].startAnimation(new RatingAnimation());
     }
 
     @Override
@@ -33,6 +50,34 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(this, AboutActivity.class));
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class RatingAnimation extends Animation {
+
+        public RatingAnimation() {
+            setDuration(mDecimalRatingBars[0].getNumStars()
+                    * 4 * getResources().getInteger(android.R.integer.config_longAnimTime));
+            setInterpolator(new LinearInterpolator());
+            setRepeatCount(Animation.INFINITE);
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            int progress = Math.round(interpolatedTime * mDecimalRatingBars[0].getMax());
+            for (RatingBar ratingBar : mDecimalRatingBars) {
+                ratingBar.setProgress(progress);
+            }
+        }
+
+        @Override
+        public boolean willChangeTransformationMatrix() {
+            return false;
+        }
+
+        @Override
+        public boolean willChangeBounds() {
+            return false;
         }
     }
 }
